d9137e1a50c2195c5f6cc74086516d433c8fada8
William Miao <wimiao@microsoft.com>
2017-07-16 14:46:11 -0700

    finished contacts

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
-
-const contacts = [
-    {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-    },
-    {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-    },
-    {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-    }
-]
+import CreateContact from './CreateContact'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
+    state = {
+        contacts: []
+    }
+    componentDidMount() {
+        ContactsAPI.getAll().then((contacts) => {
+            this.setState({ contacts })
+        })
+    }
+    removeContact = (contact) => {
+        this.setState((state) => ({
+            contacts: state.contacts.filter((c) => c.id !== contact.id)
+        }))
+
+        ContactsAPI.remove(contact)
+    }
+
+    createContact(contact) {
+        ContactsAPI.create(contact).then(contact => {
+            this.setState(state => ({
+                contacts: state.contacts.concat([ contact ])
+            }))
+        })
+    }
+
     render() {
         return (
-            <div>
-                <ListContacts contacts = {contacts} /> 
+            <div className="app">
+                <Route exact path="/" render={() => (
+                    <ListContacts 
+                        contacts = {this.state.contacts} 
+                        onDeleteContact ={this.removeContact} 
+                    /> 
+
+                )}/>
+            <Route path="/create" render={() => (
+                <CreateContact
+                    onCreateContact={(contact) => {
+                        this.createContact(contact)
+                    }}
+                />
+            )}/>
             </div>
         );
     }
